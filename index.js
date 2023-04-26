ed5fc4955f9f18bc43f22e947a873d1f1f84fcc8
mirandalu2020 <yuanling1007@hotmail.com>
2023-04-26 16:09:15 -0700

    handle get updated

--- index.js
+++ index.js
@@ -1,29 +1,23 @@
 const dynamoose = require('dynamoose');
 const personSchema = new dynamoose.Schema({
-  id: String,
-  name: String, 
-  age: String, 
-  height: String,
-})
-
+  name: String,
+  age: String,
+  height: String
+});
 const PersonModel = dynamoose.model('People', personSchema);
-
-export const handler = async(event) => {
-
-  let id = event.pathParameters;
-  let responseBody;
-
-  if(id){
-    responseBody = await PersonModel.query('id').eq(id['id']).exec();
-  } else{
+exports.handler = async(event) => {
+  console.log('READ Person EVENT: ', event);
+  // TODO implement
+  let params = event.pathParameters;
+  let responseBody = null;
+  if (params) {
+    responseBody = await PersonModel.scan('id').eq(params['id']).exec();
+  } else {
     responseBody = await PersonModel.scan().exec();
   }
-
-  console.log('PERSON REQUESTED: ', responseBody);
-
-    const response = {
-        statusCode: 200,
-        body: JSON.stringify(responseBody),
-    };
-    return response;
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify(responseBody),
+  };
+  return response;
 };
