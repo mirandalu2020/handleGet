fa0388c60c528c60781061f6ec35ed0fabb4e82a
mirandalu2020 <yuanling1007@hotmail.com>
2023-06-06 12:09:26 -0700

    plant get completed

--- index.js
+++ index.js
@@ -1,19 +1,37 @@
 const dynamoose = require('dynamoose');
-const personSchema = new dynamoose.Schema({
-  name: String,
-  age: String,
-  height: String
+const plantSchema = new dynamoose.Schema({
+  id: {
+    type: String,
+    required: true,
+  },
+  soilMoisture: {
+    type: Number,
+  },
+  temperature: {
+    type: Number,
+  },
+  humidity: {
+    type: Number,
+  },
+  timestamp: {
+    type: String,
+    required: true,
+  },
+  plantId: {
+    type: String,
+  },
+  
 });
-const PersonModel = dynamoose.model('People', personSchema);
+const plantModel = dynamoose.model('caterpillar-plant-status', plantSchema);
 exports.handler = async(event) => {
-  console.log('READ Person EVENT: ', event);
+  console.log('READ PLANT EVENT: ', event);
   // TODO implement
   let params = event.pathParameters;
   let responseBody = null;
   if (params) {
-    responseBody = await PersonModel.scan('id').eq(params['id']).exec();
+    responseBody = await plantModel.scan('id').eq(params['id']).exec();
   } else {
-    responseBody = await PersonModel.scan().exec();
+    responseBody = await plantModel.scan().exec();
   }
   const response = {
     statusCode: 200,
